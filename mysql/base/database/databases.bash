f59db2180b0f871eb5e05209f5653d6a15832edb
cesvh <cesar.vega.hdez@gmail.com>
2024-04-18 00:24:50 -0600

    MySql database

---
+++ mysql/base/database/databases.bash
@@ -0,0 +1,59 @@
+Enter password:
+Welcome to the MySQL monitor.  Commands end with ; or \g.
+Your MySQL connection id is 9
+Server version: 8.2.0 MySQL Community Server - GPL
+
+Copyright (c) 2000, 2023, Oracle and/or its affiliates.
+
+Oracle is a registered trademark of Oracle Corporation and/or its
+affiliates. Other names may be trademarks of their respective
+owners.
+
+Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
+
+mysql>
+mysql>
+mysql>
+mysql>
+mysql>
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+4 rows in set (0.01 sec)
+
+mysql>
+mysql>
+mysql>
+mysql>
+mysql>
+mysql> create database db_sqlserver_sintax;
+Query OK, 1 row affected (0.01 sec)
+
+mysql>
+mysql>
+mysql>
+mysql>
+mysql>
+mysql> show databases;
++---------------------+
+| Database            |
++---------------------+
+| db_sqlserver_sintax |
+| information_schema  |
+| mysql               |
+| performance_schema  |
+| sys                 |
++---------------------+
+5 rows in set (0.00 sec)
+
+mysql>
+mysql>
+mysql>
+mysql>
+mysql>
---
+++ mysql/base/database/databases.sql
@@ -0,0 +1,12 @@
+
+-- 
+show databases;
+
+-- Crear base de datos
+create database db_sqlserver_sintax;
+
+-- Definimos la base de datos que estaremos usando en adelante
+use db_sqlserver_sintax;
+
+-- 
+create database db_sqlserver_sintax;
