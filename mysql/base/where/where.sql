233c4a9fcc0b3cd1ef67eee1dd6466240c511624
cesvh <cesar.vega.hdez@gmail.com>
2024-04-30 23:58:45 -0600

    mysql - where

---
+++ mysql/base/where/where.sql
@@ -0,0 +1,5 @@
+-- where
+select * from tb_template where id = 1;
+select * from tb_template where int_int_3 != 1;
+select * from tb_template where vrc_varchar like '%aa';
+select * from tb_template where vrc_varchar like 'aa%';
