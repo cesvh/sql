a11b3b5d0649618cbbd2f1e7d7f4b0dac1402b62
cesvh <cesar.vega.hdez@gmail.com>
2024-04-30 23:59:45 -0600

    mysql - where is null
    select * from tb_template where vrc_varchar_2 is null;
    select * from tb_template where vrc_varchar_2 is not null;

---
+++ mysql/base/where-is-null/where-is-null.sql
@@ -0,0 +1,7 @@
+
+show databases;
+use db_sqlserver_sintax;
+show tables;
+select * from tb_template;
+select * from tb_template where vrc_varchar_2 is null;
+select * from tb_template where vrc_varchar_2 is not null;
