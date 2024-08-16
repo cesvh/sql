51d05dbe94be2e0143de87d8c31b6dcceae36fac
cesvh <cesar.vega.hdez@gmail.com>
2024-08-15 23:55:26 -0600

    select top

---
+++ sqlserver/003-Limiting-rows/002-SELECT-TOP.sql
@@ -0,0 +1,13 @@
+-- La cláusula SELECT TOP permite limitar la cantidad de filas o el porcentaje de filas que se devuelven en un conjunto de resultados de una consulta.
+
+-- SELECT TOP (expression) [PERCENT] [WITH TIES] FROM table_name ORDER BY column_name;
+-- Después de la palabra clave TOP hay una expresión que especifica el número de filas que se devolverán. La expresión se evalúa como un valor flotante si se usa PERCENT; de lo contrario, se convierte en un valor BIGINT.
+-- CON TIES le permite devolver filas adicionales con valores que coinciden con los de la última fila en el conjunto de resultados limitado. Tenga en cuenta que CON TIES puede provocar que se devuelvan más filas de las especificadas en la expresión.
+
+SELECT TOP 10 product_name, list_price FROM production.products ORDER BY list_price DESC;
+
+SELECT TOP 1 PERCENT product_name, list_price FROM production.products ORDER BY list_price DESC;
+
+SELECT TOP 3 WITH TIES product_name, list_price FROM production.products ORDER BY list_price DESC;
+
+-- En este ejemplo, el tercer producto caro tiene un precio de lista de 6499,99. Debido a que la declaración usa TOP CON CORBATAS, devuelve tres productos más cuyos precios de lista son los mismos que los del tercero.
