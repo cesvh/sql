958322b25f38c8baf7c9eb91a28149c79c822d70
cesvh <96280230+cesvh@users.noreply.github.com>
2024-10-14 16:01:08 -0600

    ...

--- sqlserver/003-Limiting-rows/001-OFFSET-FETCH.sql
+++ sqlserver/003-Limiting-rows/001-OFFSET-FETCH.sql
@@ -1,15 +1,15 @@
-ORDER BY column_list [ASC |DESC]
-OFFSET offset_row_count {ROW | ROWS}
-FETCH {FIRST | NEXT} fetch_row_count {ROW | ROWS} ONLY
+-- ORDER BY column_list [ASC |DESC]
+-- OFFSET offset_row_count {ROW | ROWS}
+-- FETCH {FIRST | NEXT} fetch_row_count {ROW | ROWS} ONLY
 -- La cláusula OFFSET especifica la cantidad de filas que se deben omitir antes de comenzar a devolver filas de la consulta. offset_row_count puede ser una constante, variable o parámetro que sea mayor o igual a cero.
 
 -- La cláusula FETCH especifica la cantidad de filas que se deben devolver después de que se haya procesado la cláusula OFFSET. offset_row_count puede ser una constante, variable o escalar que sea mayor o igual a uno.
 
 -- La cláusula OFFSET es obligatoria, mientras que la cláusula FETCH es opcional. Además, FIRST y NEXT son sinónimos, por lo que puede utilizarlos indistintamente. De manera similar, puede utilizar FIRST y NEXT
 
--- -----------------------------------------------------------------
--- -----------------------------------------------------------------
--- -----------------------------------------------------------------
+-- --------------------------------------------------------------
+-- --------------------------------------------------------------
+-- --------------------------------------------------------------
 
 -- La siguiente consulta utiliza una instrucción SELECT para recuperar todas las filas de la tabla de productos y las ordena por precios de lista y nombres:
 SELECT product_id, product_name, list_price FROM production.products ORDER BY list_price, product_name;
