ac3be2c989f4c423571a01ee8c5bd79686dbdb1a
cesvh <cesar.vega.hdez@gmail.com>
2025-07-27 00:02:36 -0600

    fix: sql select

--- 001-sql/sqlserver/001-basico/001-Querying-data/001-select.sql
+++ 001-sql/sqlserver/001-basico/001-Querying-data/001-select.sql
@@ -13,9 +13,8 @@ SELECT * FROM sales.customers WHERE state = 'CA';
 SELECT * FROM sales.customers WHERE state = 'CA' ORDER BY first_name;
 -- Ordenar filas en el conjunto de resultados por una o más columnas.
 
+-- FROM -> WHERE -> GROUP BY -> SELECT -> ORDER BY.
 SELECT city, COUNT (*) AS 'cantidad' FROM sales.customers WHERE state = 'CA' GROUP BY city ORDER BY city;
 -- Agrupar filas en grupos.
 
--- FROM -> WHERE -> GROUP BY -> SELECT ->ORDER BY.
 SELECT city, COUNT (*) FROM sales.customers WHERE state = 'CA' GROUP BY city HAVING COUNT (*) > 10 ORDER BY city;
--- Filtrar grupos
