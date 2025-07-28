b2e1612d4a937dc1c97f88894ea90298c02e0560
cesvh <cesar.vega.hdez@gmail.com>
2025-07-27 23:02:17 -0600

    fix: sql offset 10 rows fetch next 10 rows only

--- 001-sql/sqlserver/001-basico/003-Limiting-rows/001-OFFSET-FETCH.sql
+++ 001-sql/sqlserver/001-basico/003-Limiting-rows/001-OFFSET-FETCH.sql
@@ -1,6 +1,6 @@
--- La cláusula OFFSET especifica la cantidad de filas que se deben omitir antes de comenzar a devolver filas de la consulta. offset_row_count puede ser una constante, variable o parámetro que sea mayor o igual a cero.
+-- La cláusula OFFSET especifica la cantidad de filas que se deben omitir antes de comenzar a devolver filas de la consulta.
 
--- La cláusula FETCH especifica la cantidad de filas que se deben devolver después de que se haya procesado la cláusula OFFSET. offset_row_count puede ser una constante, variable o escalar que sea mayor o igual a uno.
+-- La cláusula FETCH especifica la cantidad de filas que se deben devolver después de que se haya procesado la cláusula OFFSET.
 
 -- La cláusula OFFSET es obligatoria, mientras que la cláusula FETCH es opcional. Además, FIRST y NEXT son sinónimos, por lo que puede utilizarlos indistintamente. De manera similar, puede utilizar FIRST y NEXT
 
@@ -9,14 +9,31 @@
 -- --------------------------------------------------------------
 
 -- La siguiente consulta utiliza una instrucción SELECT para recuperar todas las filas de la tabla de productos y las ordena por precios de lista y nombres:
-SELECT product_id, product_name, list_price FROM production.products ORDER BY list_price DESC, product_name;
+SELECT product_id, product_name, list_price 
+FROM production.products 
+ORDER BY list_price DESC, product_name; -- 321 registros
 
 -- Para omitir los primeros 10 productos (Desplazarce 10 registros) y devolver el resto, utilice la cláusula OFFSET como se muestra en la siguiente declaración:
-SELECT product_id, product_name, list_price FROM production.products ORDER BY list_price DESC, product_name OFFSET 10 ROWS;
+SELECT product_id, product_name, list_price 
+FROM production.products 
+ORDER BY list_price DESC, product_name 
+OFFSET 10 ROWS; -- 311 registros
 
 -- Para omitir los primeros 10 productos y seleccionar los siguientes 10 productos (Recuperar), utilice las cláusulas OFFSET y FETCH de la siguiente manera:
-SELECT product_id, product_name, list_price FROM production.products ORDER BY list_price DESC, product_name OFFSET 10 ROWS FETCH NEXT 10 ROWS ONLY;
+SELECT product_id, product_name, list_price 
+FROM production.products 
+ORDER BY list_price DESC, product_name 
+OFFSET 10 ROWS FETCH NEXT 10 ROWS ONLY;
 
 -- Usar la cláusula OFFSET FETCH para obtener las N filas superiores
 -- El siguiente ejemplo utiliza la cláusula OFFSET FETCH para recuperar los 10 productos más caros de la tabla de productos:
-SELECT product_id, product_name, list_price FROM production.products ORDER BY list_price DESC, product_name OFFSET 0 ROWS FETCH FIRST 10 ROWS ONLY;
+SELECT product_id, product_name, list_price 
+FROM production.products 
+ORDER BY list_price DESC, product_name 
+OFFSET 0 ROWS FETCH FIRST 10 ROWS ONLY;
+
+-- Primeros 10 registros agregados
+SELECT product_id, product_name, list_price 
+FROM production.products 
+ORDER BY product_id 
+OFFSET 0 ROWS FETCH FIRST 10 ROWS ONLY;
