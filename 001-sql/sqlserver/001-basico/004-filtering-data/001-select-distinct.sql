f8a4fc1148c5bf162a4a60258975a35aa7b3e687
cesvh <cesar.vega.hdez@gmail.com>
2025-07-30 20:25:22 -0600

    fix: sql select distinct

--- 001-sql/sqlserver/001-basico/004-filtering-data/001-select-distinct.sql
+++ 001-sql/sqlserver/001-basico/004-filtering-data/001-select-distinct.sql
@@ -1,23 +1,24 @@
+-- Todos los campos
+SELECT * FROM sales.customers;
+--customer_id	first_name	last_name	phone	email	street	city	state	zip_code
+
 --
 SELECT city FROM sales.customers ORDER BY city;
 
 -- Uso de SELECT DISTINCT con una columna
-SELECT DISTINCT city FROM sales.customers ORDER BY city;
-
-SELECT city, state FROM sales.customers ORDER BY city, state;
+-- Todo campo que está después de order by debe estar en el distinct
+SELECT DISTINCT state FROM sales.customers ORDER BY state;
 
 -- Uso de SELECT DISTINCT con múltiples columnas
-SELECT DISTINCT city, state FROM sales.customers;
+SELECT DISTINCT state, city FROM sales.customers;
 
 -- Uso de SELECT DISTINCT con NULL
 SELECT DISTINCT phone FROM sales.customers ORDER BY phone;
 
 -- DISTINCT vs. GROUP BY
-SELECT city, state, zip_code FROM sales.customers GROUP BY city, state, zip_code ORDER BY city, state, zip_code;
-
+SELECT city, state, zip_code FROM sales.customers GROUP BY city, state, zip_code ORDER BY state, city, zip_code;
 --
 SELECT DISTINCT city, state, zip_code FROM sales.customers;
 
 -- Tanto la cláusula DISTINCT como la GROUP BY reducen la cantidad de filas devueltas en el conjunto de resultados al eliminar los duplicados.
-
 -- Sin embargo, debe utilizar la cláusula GROUP BY cuando desee aplicar una "función de agregación" a una o más columnas.
