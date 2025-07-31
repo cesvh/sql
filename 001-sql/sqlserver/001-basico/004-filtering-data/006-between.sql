ddc5ad1ce2e426e1aa220c68cf436d914787e955
cesvh <cesar.vega.hdez@gmail.com>
2025-07-30 20:32:58 -0600

    fix: sql between

--- 001-sql/sqlserver/001-basico/004-filtering-data/006-between.sql
+++ 001-sql/sqlserver/001-basico/004-filtering-data/006-between.sql
@@ -3,3 +3,7 @@ WHERE list_price BETWEEN 149.99 AND 199.99 ORDER BY list_price;
 
 SELECT order_id, customer_id, order_date, order_status FROM sales.orders
 WHERE order_date BETWEEN '20170115' AND '20170117' ORDER BY order_date; -- Formato: YYYYMMDD
+
+-- Utilice el operador BETWEEN para seleccionar filas que tengan valores entre dos valores especificados, incluyendo los valores especificados.
+
+-- Utilice el operador NOT BETWEEN para seleccionar filas que tengan valores fuera de un rango especificado.
