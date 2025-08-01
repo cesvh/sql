25cb3b617890c2b81c460cb8c8631b958b26d9a5
cesvh <cesar.vega.hdez@gmail.com>
2025-07-31 21:22:24 -0600

    fix: sql alias

--- 001-sql/sqlserver/001-basico/004-filtering-data/008-alias.sql
+++ 001-sql/sqlserver/001-basico/004-filtering-data/008-alias.sql
@@ -1,18 +1,39 @@
-SELECT first_name + ' ' + last_name FROM sales.customers ORDER BY first_name;
 
-SELECT first_name + ' ' + last_name AS full_name FROM sales.customers ORDER BY first_name;
 
-SELECT first_name + ' ' + last_name AS 'Full Name' FROM sales.customers ORDER BY first_name;
+SELECT first_name + ' ' + last_name 
+FROM sales.customers 
+ORDER BY first_name;
 
-SELECT category_name 'Product Category' FROM production.categories ORDER BY category_name;
 
-SELECT category_name 'Product Category' FROM production.categories ORDER BY 'Product Category';
+SELECT first_name + ' ' + last_name AS full_name 
+FROM sales.customers 
+ORDER BY first_name;
 
-SELECT sales.customers.customer_id, first_name, last_name, order_id FROM sales.customers
+
+SELECT first_name + ' ' + last_name AS 'Full Name' 
+FROM sales.customers 
+ORDER BY first_name;
+
+
+SELECT category_name 'Product Category' 
+FROM production.categories 
+ORDER BY category_name;
+
+
+SELECT category_name 'Product Category' 
+FROM production.categories 
+ORDER BY 'Product Category';
+
+
+SELECT sales.customers.customer_id, first_name, last_name, order_id 
+FROM sales.customers
 INNER JOIN sales.orders ON sales.orders.customer_id = sales.customers.customer_id;
 
-SELECT c.customer_id, first_name, last_name, order_id FROM sales.customers c
+
+SELECT c.customer_id, first_name, last_name, order_id 
+FROM sales.customers c
 INNER JOIN sales.orders o ON o.customer_id = c.customer_id;
+
 
 -- Un alias de columna es un nombre temporal asignado a una columna o expresión en el conjunto de resultados de una consulta.
 -- Utilice un alias de columna para cambiar el nombre de la salida de una columna o una expresión para que tenga más significado.
