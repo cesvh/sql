2292d13c4a7898806a4647abe01d8ed8fc75defa
cesvh <cesar.vega.hdez@gmail.com>
2025-07-31 21:57:16 -0600

    feat: SQL SERVER join's

--- 001-sql/sqlserver/001-basico/004-filtering-data/007-like.sql
+++ 001-sql/sqlserver/001-basico/004-filtering-data/007-like.sql
@@ -9,37 +9,72 @@
 
 -- 1) Ejemplos de uso del operador LIKE con el comodín %
 -- last_name que inicien con z
-SELECT customer_id, first_name, last_name FROM sales.customers
-WHERE last_name LIKE 'z%' ORDER BY first_name;
+SELECT customer_id, first_name, last_name 
+FROM sales.customers
+WHERE last_name LIKE 'z%' 
+ORDER BY first_name;
+
+
 -- last_name que terminen con er
-SELECT customer_id, first_name, last_name FROM sales.customers
-WHERE last_name LIKE '%er' ORDER BY first_name;
+SELECT customer_id, first_name, last_name 
+FROM sales.customers
+WHERE last_name LIKE '%er' 
+ORDER BY first_name;
+
+
 -- last_name que inicien con t y terminen con s
-SELECT customer_id, first_name, last_name FROM sales.customers
-WHERE last_name LIKE 't%s' ORDER BY first_name;
+SELECT customer_id, first_name, last_name 
+FROM sales.customers
+WHERE last_name LIKE 't%s' 
+ORDER BY first_name;
+
+
 -- 2) Ejemplo de uso del operador LIKE con el comodín _ (guión bajo)
 -- last_name cuyo segundo caracter es u y el resto lo que sea
-SELECT customer_id, first_name, last_name FROM sales.customers
-WHERE last_name LIKE '_u%' ORDER BY first_name;
+SELECT customer_id, first_name, last_name 
+FROM sales.customers
+WHERE last_name LIKE '_u%' 
+ORDER BY first_name;
+
+
 -- 3) Ejemplo de uso del operador LIKE con el comodín [lista de caracteres]
 -- last_name que inicien con Y o Z
-SELECT customer_id, first_name, last_name FROM sales.customers
-WHERE last_name LIKE '[YZ]%' ORDER BY last_name;
+SELECT customer_id, first_name, last_name 
+FROM sales.customers
+WHERE last_name LIKE '[YZ]%' 
+ORDER BY last_name;
+
+
 -- 4) Ejemplo de uso del operador LIKE con el comodín [carácter-carácter]
 -- last_name que inicien con A hasta E
-SELECT customer_id, first_name, last_name FROM sales.customers
-WHERE last_name LIKE '[A-E]%' ORDER BY first_name;
+SELECT customer_id, first_name, last_name 
+FROM sales.customers
+WHERE last_name LIKE '[A-E]%' 
+ORDER BY last_name;
+
+
 -- 5) Ejemplo de uso del operador LIKE con el comodín [^Lista de caracteres o rango]
 -- last_name que no inicien con A hasta X
-SELECT customer_id, first_name, last_name FROM sales.customers
-WHERE last_name LIKE '[^A-X]%' ORDER BY last_name;
+SELECT customer_id, first_name, last_name 
+FROM sales.customers
+WHERE last_name LIKE '[^A-X]%' 
+ORDER BY last_name;
+
+
 -- 6) Usando el ejemplo del operador NOT LIKE
 -- first_name que no inicien con A
-SELECT customer_id, first_name, last_name FROM sales.customers
-WHERE first_name NOT LIKE 'A%' ORDER BY first_name;
+SELECT customer_id, first_name, last_name 
+FROM sales.customers
+WHERE first_name NOT LIKE 'A%' 
+ORDER BY first_name;
+
+
 -- 7) Ejemplo de uso del operador LIKE con ESCAPE
 -- Escapamos el caracter % para dar con 30%
-SELECT feedback_id, comment FROM sales.feedbacks
-WHERE comment LIKE '%30!%%' ESCAPE '!';
+--SELECT feedback_id, comment 
+--FROM sales.feedbacks
+--WHERE comment LIKE '%30!%%' 
+--ESCAPE '!';
+
 
 -- Utilice el operador LIKE para comprobar si un valor coincide con un patrón especificado.
--- 001-sql/sqlserver/001-basico/005-joins/001-joins.sql
+++ 001-sql/sqlserver/001-basico/005-joins/001-joins.sql
@@ -1,48 +1,73 @@
 USE BikeStores;
+go
 
-CREATE SCHEMA hr;
-GO
+--CREATE SCHEMA hr;
+--GO
 
-CREATE TABLE hr.candidates(
-    id INT PRIMARY KEY IDENTITY,
-    fullname VARCHAR(100) NOT NULL
-);
+--CREATE TABLE hr.candidates(
+--    id INT PRIMARY KEY IDENTITY,
+--    fullname VARCHAR(100) NOT NULL
+--);
+--go
 
-CREATE TABLE hr.employees(
-    id INT PRIMARY KEY IDENTITY,
-    fullname VARCHAR(100) NOT NULL
-);
+--CREATE TABLE hr.employees(
+--    id INT PRIMARY KEY IDENTITY,
+--    fullname VARCHAR(100) NOT NULL
+--);
+--go
 
-INSERT INTO hr.candidates(fullname) VALUES ('John Doe'), ('Lily Bush'), ('Peter Drucker'), ('Jane Doe');
-INSERT INTO hr.employees(fullname) VALUES ('John Doe'), ('Jane Doe'), ('Michael Scott'), ('Jack Sparrow');
+--INSERT INTO hr.candidates(fullname) 
+--VALUES ('John Doe'), ('Lily Bush'), ('Peter Drucker'), ('Jane Doe');
+--INSERT INTO hr.employees(fullname) 
+--VALUES ('John Doe'), ('Jane Doe'), ('Michael Scott'), ('Jack Sparrow');
+
 
 select * from hr.candidates;
 select * from hr.employees;
 
+
 -- Produce un conjunto de datos que incluye filas de la tabla izquierda y filas coincidentes de la tabla derecha.
 SELECT c.id candidate_id, c.fullname candidate_name, e.id employee_id, e.fullname employee_name
-FROM hr.candidates c INNER JOIN hr.employees e ON e.fullname = c.fullname;
+FROM hr.candidates c 
+INNER JOIN hr.employees e ON e.fullname = c.fullname;
+
 
 -- Selecciona datos de la tabla izquierda y las filas coincidentes de la tabla derecha. La unión izquierda devuelve todas las filas de la tabla izquierda y las filas coincidentes de la tabla derecha. Si una fila de la tabla izquierda no coincide con una fila de la tabla derecha, las columnas de la tabla derecha tendrán valores nulos.
 SELECT c.id candidate_id, c.fullname candidate_name, e.id employee_id, e.fullname employee_name
-FROM hr.candidates c LEFT JOIN hr.employees e ON e.fullname = c.fullname;
+FROM hr.candidates c 
+LEFT JOIN hr.employees e ON e.fullname = c.fullname;
+
 
 -- Para obtener las filas que están disponibles solo en la tabla izquierda pero no en la tabla derecha, agregue una cláusula WHERE a la consulta anterior:
 SELECT c.id candidate_id, c.fullname candidate_name, e.id employee_id, e.fullname employee_name
-FROM hr.candidates c LEFT JOIN hr.employees e ON e.fullname = c.fullname WHERE e.id IS NULL;
+FROM hr.candidates c 
+LEFT JOIN hr.employees e ON e.fullname = c.fullname 
+WHERE e.id IS NULL;
+
 
 -- Devuelve un conjunto de resultados que contiene todas las filas de la tabla derecha y las filas coincidentes de la tabla izquierda. Si una fila de la tabla derecha no coincide con una fila de la tabla izquierda, todas las columnas de esta contendrán valores nulos.
 SELECT c.id candidate_id, c.fullname candidate_name, e.id employee_id, e.fullname employee_name
-FROM hr.candidates c RIGHT JOIN hr.employees e ON e.fullname = c.fullname;
+FROM hr.candidates c 
+RIGHT JOIN hr.employees e ON e.fullname = c.fullname;
+
 
 -- De manera similar, puede obtener filas que están disponibles solo en la tabla correcta agregando una cláusula WHERE a la consulta anterior de la siguiente manera:
 SELECT c.id candidate_id, c.fullname candidate_name, e.id employee_id, e.fullname employee_name
-FROM hr.candidates c RIGHT JOIN hr.employees e ON e.fullname = c.fullname WHERE c.id IS NULL;
+FROM hr.candidates c 
+RIGHT JOIN hr.employees e ON e.fullname = c.fullname 
+WHERE c.id IS NULL;
+
 
 -- La unión externa completa o la unión completa devuelven un conjunto de resultados que contiene todas las filas de las tablas izquierda y derecha, con las filas coincidentes de ambos lados, si están disponibles. Si no hay coincidencia, el lado faltante tendrá valores nulos.
 SELECT c.id candidate_id, c.fullname candidate_name, e.id employee_id, e.fullname employee_name
-FROM hr.candidates c FULL JOIN hr.employees e ON e.fullname = c.fullname;
+FROM hr.candidates c 
+FULL JOIN hr.employees e ON e.fullname = c.fullname;
+
 
 -- Para seleccionar filas que existen en la tabla izquierda o derecha, excluya las filas que sean comunes a ambas tablas agregando una cláusula WHERE como se muestra en la siguiente consulta:
 SELECT c.id candidate_id, c.fullname candidate_name, e.id employee_id, e.fullname employee_name
-FROM hr.candidates c FULL JOIN hr.employees e ON e.fullname = c.fullname WHERE c.id IS NULL OR e.id IS NULL;
+FROM hr.candidates c 
+FULL JOIN hr.employees e ON e.fullname = c.fullname 
+WHERE c.id IS NULL OR e.id IS NULL;
+
+go
