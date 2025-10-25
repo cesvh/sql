a83264197cab1d7a4b42130b0b0bd98714023d70
cesvh <cesar.vega.hdez@gmail.com>
2025-10-24 22:14:07 -0600

    fix: sql server, refactorización de left join

--- 001-sql/sqlserver/001-basico/005-joins/003-left-join.sql
+++ 001-sql/sqlserver/001-basico/005-joins/003-left-join.sql
@@ -3,27 +3,48 @@
 -- En resumen, la cláusula LEFT JOIN devuelve todas las filas de la tabla izquierda (T1) y las filas coincidentes o valores NULL de la tabla derecha (T2).
 
 
-SELECT product_name, order_id 
-FROM production.products p
-LEFT JOIN sales.order_items o ON o.product_id = p.product_id 
-ORDER BY order_id;
+SELECT 
+	product_name, order_id 
+FROM 
+	production.products p
+LEFT JOIN 
+	sales.order_items o ON o.product_id = p.product_id 
+ORDER BY 
+	order_id;
 
 
-SELECT product_name, order_id 
-FROM production.products p
-LEFT JOIN sales.order_items o ON o.product_id = p.product_id 
-WHERE order_id IS NULL;
+SELECT 
+	product_name, order_id 
+FROM 
+	production.products p
+LEFT JOIN 
+	sales.order_items o ON o.product_id = p.product_id 
+WHERE 
+	order_id IS NULL;
 
 
-SELECT p.product_name, o.order_id, i.item_id, o.order_date
-FROM production.products p
-LEFT JOIN sales.order_items i ON i.product_id = p.product_id
-LEFT JOIN sales.orders o ON o.order_id = i.order_id
-ORDER BY order_id;
+SELECT 
+	p.product_name
+	, o.order_id
+	, i.item_id
+	, o.order_date
+FROM 
+	production.products p
+LEFT JOIN 
+	sales.order_items i ON i.product_id = p.product_id
+LEFT JOIN 
+	sales.orders o ON o.order_id = i.order_id
+ORDER BY 
+	order_id;
 
 
-SELECT product_name, order_id 
-FROM production.products p
-LEFT JOIN sales.order_items o ON o.product_id = p.product_id
-WHERE order_id = 100 
-ORDER BY order_id;
+SELECT 
+	product_name, order_id 
+FROM 
+	production.products p
+LEFT JOIN 
+	sales.order_items o ON o.product_id = p.product_id
+WHERE 
+	order_id = 100 
+ORDER BY 
+	order_id;
