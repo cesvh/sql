0839d731c815efd1e15f2c2c7a63a9e4cafd0f49
cesvh <cesar.vega.hdez@gmail.com>
2026-06-27 12:33:30 -0600

    fix: sqlserver select @@version

--- 001-sql/sqlserver/000-Getting-Started-with-SQL-Server/001-select-version.sql
+++ 001-sql/sqlserver/000-Getting-Started-with-SQL-Server/001-select-version.sql
@@ -4,3 +4,13 @@ GO
 /*
 Microsoft SQL Server 2022 (RTM-GDR) (KB5091158) - 16.0.1180.1 (X64)   Apr 16 2026 14:18:38   Copyright (C) 2022 Microsoft Corporation  Developer Edition (64-bit) on Windows 10 Pro 10.0 <X64> (Build 26200: ) (Hypervisor) 
 */
+
+/*
+SQL Server Management Studio						21.6.17+1.36603.0
+Objetos de administración de SQL Server (SMO)						17.100.73.0+e183799e249c9bb0ba9f7e74c4e23007e11dabd7
+Analizador de T-SQL Microsoft						17.2.22.0+0df7893883f565f6d690e7272ae7e1b5e7872b2b.0df7893883f565f6d690e7272ae7e1b5e7872b2b
+Microsoft Data SqlClient (MDS)						5.1.6
+Microsoft SQL Server Data-Tier Application Framework (DacFX)						170.0.96.2+38131d374cd6c4991a9ada0c74d79de3392fda0b
+Microsoft .NET Framework						4.0.30319.42000
+Sistema operativo						10.0.26200
+*/
