06563b51f9c8f82ee91214f979e52709f3f874bc
cesvh <cesar.vega.hdez@gmail.com>
2026-06-26 22:46:02 -0600

    fix: sqlserver order by

---
+++ 001-sql/sqlserver/001-basico/002-Sorting-data/001-bikesstores/001-order-by.sql
@@ -0,0 +1,14 @@
+
+SELECT * FROM [sales].[customers] ORDER BY first_name;
+
+SELECT * FROM [sales].[customers] ORDER BY first_name DESC;
+
+SELECT * FROM [sales].[customers] ORDER BY city, first_name;
+
+SELECT * FROM [sales].[customers] ORDER BY city DESC, first_name ASC;
+
+SELECT * FROM [sales].[customers] ORDER BY LEN(first_name) DESC;
+
+SELECT * FROM [sales].[customers] ORDER BY 2, 3;
+
+select * from [sales].[customers] order by email;
